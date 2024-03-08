6f8cef0de90b70786b034a494ada41d1b1999e80
Gabriel Bibbó <80938476+gbibbo@users.noreply.github.com>
2024-03-08 13:12:19 +0000

    Update run_sed_demo.sh

--- run_sed_demo.sh
+++ run_sed_demo.sh
@@ -4,28 +4,27 @@
 echo "Script started at $(date)" >> /home/ai4s/pisoundsensing/sed_demo.log
 
 # Set the PYTHONPATH
-# export PYTHONPATH="/home/pi/Downloads/General-Purpose-Sound-Recognition-Demo-master:$PYTHONPATH"
 cd /home/ai4s/pisoundsensing
 
 # Run sed_demo and log the output and errors
 python3 -m sed_demo MODEL_PATH='Cnn9_GMP_64x64_300000_iterations_mAP=0.37.pth?download=1' # >> /home/ai4s/pisoundsensing/sed_demo.log 2>&1
 
 
-# Luego, hay que hacer lo siguiente
-# Hacer que el script sea ejecutable:
+# Then, you need to do the following
+# Make the script executable:
 #
 # chmod +x ~/run_sed_demo.sh
 # 
-# Crear un archivo .desktop que se encargue de ejecutar este script al iniciar el sistema:
+# Create a .desktop file that executes this script at system startup:
 # 
 # nano ~/.config/autostart/run_sed_demo.desktop
 #
-# Agrega las siguientes líneas al archivo:
+# Add the following lines to the file:
 # 
 #[Desktop Entry]
 #Type=Application
 #Name=Run sed_demo
-#Exec=/home/pi/AI4S-demo/run_sed_demo.sh
+#Exec=/home/ai4s/pisoundsensing/run_sed_demo.sh
 
 
 
